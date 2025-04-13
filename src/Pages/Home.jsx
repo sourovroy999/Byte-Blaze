49582ff11530771f4214baea80e3f6e3afe8d657
sourovroy999 <sourovroy3050@gmail.com>
2025-04-13 12:27:24 +0600

    theme basic added

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from '../Components/Nav'
+
+const Home = () => {
+    return (
+        <div>
+            <Nav></Nav>
+            <h1>Homepage</h1>
+        </div>
+    );
+};
+
+export default Home;
